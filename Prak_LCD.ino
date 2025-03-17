a489c0831340f1a8688261d13c2be2d69e84c16d
Raffi Fadlika <150664536+mrfadlika@users.noreply.github.com>
2025-03-17 12:05:15 +0800

    Update Prak_LCD.ino

--- Prak_LCD.ino
+++ Prak_LCD.ino
@@ -1,26 +1,25 @@
 #include <LiquidCrystal.h>
 
-// Inisialisasi LCD dengan pin (RS, E, D4, D5, D6, D7)
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
 
 void setup() {
-  lcd.begin(16, 2);  // Inisialisasi LCD 16x2
+  lcd.begin(16, 2); 
 }
 
 void loop() {
-  lcd.clear();  // Menghapus layar LCD
-  lcd.setCursor(0, 0);  // Set posisi di kolom 0, baris ke-0
+  lcd.clear(); 
+  lcd.setCursor(0, 0); 
   lcd.print("Raffi Fadlika");
-  lcd.setCursor(0, 1);  // Set posisi di kolom 0, baris ke-1
+  lcd.setCursor(0, 1); 
   lcd.print("42623002");
 
-  delay(2000);  // Tampilkan selama 2 detik
+  // delay(2000);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);  // Set posisi di kolom 4, baris ke-0
-  lcd.print("Andrew Nathanael");
-  lcd.setCursor(0, 1);  // Set posisi di kolom 5, baris ke-1
-  lcd.print("42623005");
+  // lcd.clear();
+  // lcd.setCursor(0, 0);
+  // lcd.print("Andrew Nathanael");
+  // lcd.setCursor(0, 1);
+  // lcd.print("42623005");
 
-  delay(2000);  // Tampilkan selama 2 detik
+  delay(2000);
 }
